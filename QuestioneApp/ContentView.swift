b86534024f778b1ef952e0ea19b44a7dc122bbc0
waiga <hyang9387@gmail.com>
2022-04-02 13:20:40 +0900

    Initial Commit

---
+++ QuestioneApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuestioneApp
+//
+//  Created by 김미향 on 2022/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuestioneApp/QuestioneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestioneAppApp.swift
+//  QuestioneApp
+//
+//  Created by 김미향 on 2022/04/02.
+//
+
+import SwiftUI
+
+@main
+struct QuestioneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
